bb9f537b949c01335599a29f8246b763937d85bc
tzhao95 <tzhao95@gmail.com>
2018-04-17 15:10:58 -0400

    inverse kinimatics program started

---
+++ InverseKinematics.py
@@ -0,0 +1,10 @@
+import pdb
+from math import radians, sin, cos, degrees, atan2, sqrt, pi
+
+#x, y, z, O, A ,T
+inputs = []
+#Used to store radian value of OAT angle
+OAT = []
+
+for i in range(3:6):
+	OAT.append(radiands(inputs[i]))
