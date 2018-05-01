d72cbec8be08c12c3ce4faa61d529aad87a65433
tzhao95 <tzhao95@gmail.com>
2018-04-30 20:17:18 -0400

    bugs fixed

--- InverseKinematics.py
+++ InverseKinematics.py
@@ -3,7 +3,13 @@ from math import radians, sin, cos, degrees, atan2, sqrt, pi
 
 #x, y, z, O, A ,T
 #inputs = [-972.2000, -261.760, 794.338, -45.241, 4.735, -112.266]
-inputs = [648.175, 362.614, 1111.597, 112.232, -17.655, 46.079]
+#inputs = [648.175, 362.614, 1111.597, 112.232, -17.655, 46.079]
+inputs = [879.737, 414.591, 908.843, 101.208, -5.418, -65.169]
+#inputs = [295.607, -66.719, 1079.214, 38.285, -4.447, 14.899]
+
+#results 2 = [12.749, -71.495, 111.84, 16.799, 32.899, 34.755]
+#results 3 = 14.311, -58.746, 114.069, -6.303, 29.399, -59.966
+#results 4 = -51.872, -113.440, 151.385, 4.832, 47.600, 14.767
 #Used to store radian value of OAT angle
 OAT = []
 xyz = []
@@ -11,7 +17,9 @@ cOAT = []
 sOAT = []
 c = []
 s = []
-
+s6temp = []
+c6temp = []
+s23temp = []
 theta1 = []
 theta2 = []
 theta3 = []
@@ -36,7 +44,7 @@ r13 = sOAT[0]*cOAT[1]
 r21 = -1 * (sOAT[0]*sOAT[2] + cOAT[0]*sOAT[1]*cOAT[2])
 r22 = -1 * (sOAT[0]*cOAT[2] - cOAT[0]*sOAT[1]*sOAT[2])
 r23 = -1 * cOAT[0]*cOAT[1]
-r31 = -1 * cOAT[1]*cOAT[2] 
+r31 =  cOAT[1]*cOAT[2] 
 r32 = -1 * cOAT[1]*sOAT[2]
 r33 = -1 * sOAT[1]
 px = -125 * r13 + xyz[0]
@@ -56,13 +64,17 @@ def t4(t1, t3):
 	return degrees(atan2(-r13*sin(radians(t1)) + r23*cos(radians(t1)), -r13*cos(radians(t1))*c23(t1,t3) - r23*sin(radians(t1))*c23(t1,t3) + r33*s23(t1,t3)))
 
 def t5(t1, t3, t4):
-	s5 = -1 * r13*(cos(radians(t1)) * c23(t1,t3) * cos(radians(t4)) + sin(radians(t1))*sin(radians(t4))) + r23*(sin(radians(t1))*c23(t1, t3) * cos(radians(t4)) - cos(radians(t1))*sin(radians(t4))) - r33*(s23(t1, t3) * cos(radians(t4)))
+	s5 = -1 * (r13*(cos(radians(t1)) * c23(t1,t3) * cos(radians(t4)) + sin(radians(t1))*sin(radians(t4))) + r23*(sin(radians(t1))*c23(t1, t3) * cos(radians(t4)) - cos(radians(t1))*sin(radians(t4))) - r33*(s23(t1, t3) * cos(radians(t4))))
 	c5 = r13*(-cos(radians(t1))*s23(t1, t3)) + r23*(-sin(radians(t1))*s23(t1, t3)) + r33*(-c23(t1,t3))
 	return degrees(atan2(s5, c5))
 
 def t6(t1, t3, t4, t5):
 	s6 = -r11*(cos(radians(t1))*c23(t1, t3)*sin(radians(t4)) - sin(radians(t1))*cos(radians(t4))) - r21*(sin(radians(t1))*c23(t1, t3)*sin(radians(t4)) + cos(radians(t1))*cos(radians(t4))) + r31*(s23(t1, t3)*sin(radians(t4)))
-	c6 = r11*((cos(radians(t1))*c23(t1, t3)*sin(radians(t4)) + sin(radians(t1))*sin(radians(t4)))*cos(radians(t5)) - cos(radians(t1))*s23(t1, t3)*sin(radians(t5))) + r21*((sin(radians(t1))*c23(t1,t3)*cos(radians(t4)) - cos(radians(t1))*sin(radians(t4)))*cos(radians(t5)) - sin(radians(t1))*s23(t1, t3)*sin(radians(t5))) - r31(s23(t1,t3)*cos(radians(t4))*cos(radians(t5)) + c23(t1, t3)*sin(radians(t5)))
+	c6 = r11*((cos(radians(t1))*c23(t1, t3)*cos(radians(t4)) + sin(radians(t1))*sin(radians(t4)))*cos(radians(t5)) - cos(radians(t1))*s23(t1, t3)*sin(radians(t5))) + r21*((sin(radians(t1))*c23(t1,t3)*cos(radians(t4)) - cos(radians(t1))*sin(radians(t4)))*cos(radians(t5)) - sin(radians(t1))*s23(t1, t3)*sin(radians(t5))) - r31*(s23(t1,t3)*cos(radians(t4))*cos(radians(t5)) + c23(t1, t3)*sin(radians(t5)))
+	s23temp.append(s23(t1,t3))
+	s6temp.append(s6)
+	c6temp.append(c6)
+	return degrees(atan2(s6, c6))
 '''
 print('r11 = ', r11)
 print('r12 = ', r12)
@@ -82,6 +94,7 @@ print('pz = ', pz)
 theta1.append(degrees(atan2(py, px) - atan2(d[2], sqrt(px*px + py*py - d[2]*d[2]))))
 theta1.append(degrees(atan2(py, px) - atan2(d[2], -1 * sqrt(px*px + py*py - d[2]*d[2]))))
 #print('t1 = ', t1)
+print("theta1:")
 print(theta1)
 
 c1 = cos(radians(theta1[0]))
@@ -92,8 +105,12 @@ print(K)
 
 theta3.append(degrees(atan2(a[3], d[3]) - atan2(K, sqrt(a[3]*a[3] + d[3]*d[3]-K*K))))
 theta3.append(degrees(atan2(a[3], d[3]) - atan2(K, -1 * sqrt(a[3]*a[3] + d[3]*d[3]-K*K))))
+#print("theta3:")
+#print(theta3 )
 
-print(theta3)
+theta3output = [theta3[0] + 180, theta3[1] + 180]
+print('theta3:')
+print(theta3output)
 '''
 c3 = cos(radians(-theta3[1]))
 s3 = sin(radians(-theta3[1]))
@@ -102,26 +119,47 @@ theta2.append(t23(theta1[0], theta3[0]) - theta3[0])
 theta2.append(t23(theta1[1], theta3[0]) - theta3[0])
 theta2.append(t23(theta1[0], theta3[1]) - theta3[1])
 theta2.append(t23(theta1[1], theta3[1]) - theta3[1])
-
+print("theta2:")
 print(theta2)
 
 theta4.append(t4(theta1[0], theta3[0]))
 theta4.append(t4(theta1[1], theta3[0]))
 theta4.append(t4(theta1[0], theta3[1]))
 theta4.append(t4(theta1[1], theta3[1]))
-
+theta4.append(theta4[0] + 180)
+theta4.append(theta4[1] + 180)
+theta4.append(theta4[2] + 180)
+theta4.append(theta4[3] + 180)
+print("theta4:")
 print(theta4)
 
 theta5.append(t5(theta1[0], theta3[0], theta4[0]))
 theta5.append(t5(theta1[1], theta3[0], theta4[1]))
 theta5.append(t5(theta1[0], theta3[1], theta4[2]))
 theta5.append(t5(theta1[1], theta3[1], theta4[3]))
-theta5.append(t5(theta1[0], theta3[0], theta4[0]))
-theta5.append(t5(theta1[0], theta3[0], theta4[0]))
-theta5.append(t5(theta1[0], theta3[0], theta4[0]))
-theta5.append(t5(theta1[0], theta3[0], theta4[0]))
+theta5.append(-theta5[0])
+theta5.append(-theta5[1])
+theta5.append(-theta5[2])
+theta5.append(-theta5[3])
+print("theta5:")
 print(theta5)
 
+theta6.append(t6(theta1[0], theta3[0], theta4[0], theta5[0]))
+theta6.append(t6(theta1[1], theta3[0], theta4[1], theta5[1]))
+theta6.append(t6(theta1[0], theta3[1], theta4[2], theta5[2]))
+theta6.append(t6(theta1[1], theta3[1], theta4[3], theta5[3]))
+theta6.append(theta6[0] + 180)
+theta6.append(theta6[1] + 180)
+theta6.append(theta6[2] + 180)
+theta6.append(theta6[3] + 180)
+print("theta6:")
+print(theta6)
+
+'''
+print("C6: ", c6temp)
+print("S6: ", s6temp)
+print("S23: ", s23temp)
+'''
 '''
 s23 =  ((-a[3] - (a[2]*c3))*pz + (c1*px + s1*py)*(a[2]*s3 - d[3]))/(pz*pz + (c1*px + s1*py)*(c1*px + s1*py))
 c23 = ((a[2]*s3 - d[3])*pz + (a[3]+a[2]*c3)*(c1*px + s1*py))/(pz*pz + (c1*px + s1*py)*(c1*px + s1*py))
